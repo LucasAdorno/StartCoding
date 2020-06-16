bec715fa959a80b284dd8400539a812ac509fed1
LucasAdorno <lucasadorno2702@gmail.com>
2020-06-16 00:00:10 -0300

    Adicionando extras

--- scripts/selectList.js
+++ scripts/selectList.js
@@ -15,9 +15,9 @@ function ativarMarcador(){
         x++
     }
     console.log(event.target.id);
-    if(event.target.id == 0){$('iframe').attr('src', 'https://www.youtube.com/embed/w2jeYJB4zqA'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 1){$('iframe').attr('src', 'https://www.youtube.com/embed/OI2trlIS7Lc'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 2){$('iframe').attr('src', 'https://www.youtube.com/embed/hn-SEp-1kQ8'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 3){$('iframe').attr('src', 'https://www.youtube.com/embed/M9C3mdIbDLw'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 4){$('iframe').attr('src', 'https://www.youtube.com/embed/zvaYDPPKh-Q'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 0){$('iframe').attr('src', 'https://www.youtube.com/embed/w2jeYJB4zqA'); $('.download').attr('href', '/documents/StartCod_ng Lista - 01.pdf')}
+    if(event.target.id == 1){$('iframe').attr('src', 'https://www.youtube.com/embed/OI2trlIS7Lc'); $('.download').attr('href', '/documents/StartCod_ng Lista - 02.pdf')}
+    if(event.target.id == 2){$('iframe').attr('src', 'https://www.youtube.com/embed/hn-SEp-1kQ8'); $('.download').attr('href', '/documents/StartCod_ng Lista - 03.pdf')}
+    if(event.target.id == 3){$('iframe').attr('src', 'https://www.youtube.com/embed/M9C3mdIbDLw'); $('.download').attr('href', '/documents/StartCod_ng Lista - 04.pdf')}
+    if(event.target.id == 4){$('iframe').attr('src', 'https://www.youtube.com/embed/zvaYDPPKh-Q'); $('.download').attr('href', '/documents/StartCod_ng Lista - 05.pdf')}
 }

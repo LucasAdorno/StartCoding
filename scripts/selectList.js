b979a9336790a65c0c9260c48eb43d3eaf3d5f74
LucasAdorno <lucasadorno2702@gmail.com>
2020-06-15 21:05:46 -0300

    Adicionando link dos videos finais

--- scripts/selectList.js
+++ scripts/selectList.js
@@ -15,9 +15,9 @@ function ativarMarcador(){
         x++
     }
     console.log(event.target.id);
-    if(event.target.id == 0){$('iframe').attr('src', 'https://www.youtube.com/embed/0Gi-cSZetDY'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 1){$('iframe').attr('src', 'https://www.youtube.com/embed/37wV1l9iUvA'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 2){$('iframe').attr('src', 'https://www.youtube.com/embed/WPTQ8fZ0aQk'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 3){$('iframe').attr('src', 'https://www.youtube.com/embed/Gojqw9BQ5qY'); $('.download').attr('href', 'origem do download')}
-    if(event.target.id == 4){$('iframe').attr('src', 'https://www.youtube.com/embed/Q8eajxcS6dQ'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 0){$('iframe').attr('src', 'https://www.youtube.com/embed/w2jeYJB4zqA'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 1){$('iframe').attr('src', 'https://www.youtube.com/embed/OI2trlIS7Lc'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 2){$('iframe').attr('src', 'https://www.youtube.com/embed/hn-SEp-1kQ8'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 3){$('iframe').attr('src', 'https://www.youtube.com/embed/M9C3mdIbDLw'); $('.download').attr('href', 'origem do download')}
+    if(event.target.id == 4){$('iframe').attr('src', 'https://www.youtube.com/embed/zvaYDPPKh-Q'); $('.download').attr('href', 'origem do download')}
 }
